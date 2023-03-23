5dc2e7cdf967eb21cbbc0da443afcf403b077da2
Nazar Koval <n.kovaldev@gmail.com>
2023-03-23 07:19:57 +0200

    feat: countries's storage
    In this commit I have created new folder in the store and 3 files regarding countries ( actions, reducer and selectors ). And prepared everyting for api request

---
+++ src/store/countries/countries-reducer.js
@@ -0,0 +1,20 @@
+import { SET_COUNTRIES, SET_LOADING, SET_ERROR } from "./countries-actions"
+
+const initialState = {
+	status: "idle", // loading | received | rejected
+	error: null,
+	list: []
+}
+
+export const countriesReducer = (state = initialState, { type, payload }) => {
+	switch (type) {
+		case SET_COUNTRIES:
+			return { ...state, status: "received", list: payload }
+		case SET_LOADING:
+			return { ...state, status: "loading", error: null }
+		case SET_ERROR:
+			return { ...state, status: "rejected", error: payload }
+		default:
+			return state
+	}
+}
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
+import { countriesReducer } from "./countries/countries-reducer"
 import themeReducer from "./theme/theme-reducer"
 
 const rootReducer = combineReducers({
-	theme: themeReducer
+	theme: themeReducer,
+	countries: countriesReducer
 })
 
 export default rootReducer
