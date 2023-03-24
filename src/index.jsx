a4c4625fff64fb82d106bf12d08ba222eed5a9e9
Nazar Koval <n.kovaldev@gmail.com>
2023-03-24 08:51:23 +0200

    feat: add persistent

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,17 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
-import store from "./store"
+import { PersistGate } from "redux-persist/integration/react"
+import store, { persistor } from "./store"
 import App from "./App"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<Provider store={store} >
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+	<Provider store={store}>
+		<PersistGate loading={null} persistor={persistor}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</PersistGate>
 	</Provider>
 )
